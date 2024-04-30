dc5ec20cf3897fa29c589bb495ea0bbff86f593b
Yifan Jin <yijin@expedia.com>
2024-04-30 12:10:13 +1000

    Initial Commit

---
+++ tca-tree-nav/tca_tree_navApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_tree_navApp.swift
+//  tca-tree-nav
+//
+//  Created by Yifan Jin on 30/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct tca_tree_navApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
